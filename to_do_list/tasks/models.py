8337247b8af5c24b94f40e4eca9af4184e87aadc
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-03-05 23:54:42 +0300

    created Task model

--- to_do_list/tasks/models.py
+++ to_do_list/tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Tasks(models.Model):
+    title=models.CharField(max_length=200)
+
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_created=True)
+    def __str__(self):
+        return self.title
+
 # Create your models here.
